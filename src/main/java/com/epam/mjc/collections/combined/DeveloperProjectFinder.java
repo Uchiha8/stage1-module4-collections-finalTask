87f1f76573ee823fcada2ed921c02473aad6d92c
[200~2110107@newuu.uz <2110107@newuu.uz>
2025-03-02 13:07:15 +0500

    Implement methods to aggregate lessons and find developer projects

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,27 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> projectList = new ArrayList<>();
+
+        // Find projects where the developer is involved
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            if (entry.getValue().contains(developer)) {
+                projectList.add(entry.getKey());
+            }
+        }
+
+        // Sort by length descending, then alphabetically in reverse order
+        projectList.sort((a, b) -> {
+            int lengthCompare = Integer.compare(b.length(), a.length());
+            return lengthCompare != 0 ? lengthCompare : b.compareTo(a);
+        });
+
+        return projectList;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,20 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> resultMap = new HashMap<>();
+
+        for (String key : sourceMap.keySet()) {
+            int length = key.length();
+            resultMap.putIfAbsent(length, new HashSet<>());
+            resultMap.get(length).add(key);
+        }
+
+        return resultMap;
     }
 }
